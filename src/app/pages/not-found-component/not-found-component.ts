6b1c7f4893178190f647d9ec47c3779a1610f489
Hushini <wh53911@zut.edu.pl>
2026-01-07 09:49:52 +0100

    lab12

--- src/app/pages/not-found-component/not-found-component.ts
+++ src/app/pages/not-found-component/not-found-component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found-component',
+  standalone: true,
   imports: [],
   templateUrl: './not-found-component.html',
   styleUrl: './not-found-component.css',
 })
-export class NotFoundComponent {
-
-}
+export class NotFoundComponent {}
